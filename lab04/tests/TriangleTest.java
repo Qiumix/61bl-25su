8374994e61d6156079498fc4d9f596905e485d11
Qiumix <j487428862@gmail.com>
2025-08-09 22:30:32 +0800

    complete lab04/Triangle

--- lab04/tests/TriangleTest.java
+++ lab04/tests/TriangleTest.java
@@ -1,5 +1,5 @@
-import org.junit.Rule;
 import org.junit.Test;
+
 import static com.google.common.truth.Truth.assertWithMessage;
 public abstract class TriangleTest {
 
@@ -18,6 +18,65 @@ public abstract class TriangleTest {
         // remember that you'll have to call on Triangle methods like
         // t.functionName(arguments), where t is a Triangle object
 
+    }
+
+    @Test
+    public void test2() {
+        Triangle triangle = getNewTriangle();
+        assertWithMessage("actual is not expected")
+                .that(triangle.sidesFormTriangle(1, 2, 3))
+                .isEqualTo(false);
+        assertWithMessage("actual is not expected")
+                .that(triangle.sidesFormTriangle(3, 3, 3))
+                .isEqualTo(true);
+        assertWithMessage("actual is not expected")
+                .that(triangle.sidesFormTriangle(3, 4, 5))
+                .isEqualTo(true);
+        assertWithMessage("actual is not expected")
+                .that(triangle.sidesFormTriangle(0, 0, 0))
+                .isEqualTo(false);
+    }
+
+    @Test
+    public void test3() {
+        Triangle triangle = getNewTriangle();
+        assertWithMessage("actual is not expected")
+                .that(triangle.pointsFormTriangle(1, 1, 1, 1, 1, 1))
+                .isEqualTo(true);
+        assertWithMessage("actual is not expected")
+                .that(triangle.pointsFormTriangle(1, 1, 0, 0, -1, -1))
+                .isEqualTo(false);
+        assertWithMessage("actual is not expected")
+                .that(triangle.pointsFormTriangle(1, 1, 0, 0, -1, -1))
+                .isEqualTo(false);
+    }
+
+    @Test
+    public void test4() {
+        Triangle triangle = getNewTriangle();
+        assertWithMessage("actual is not expected")
+                .that(triangle.triangleType(1, 1, 1))
+                .isEqualTo("Equilateral");
+        assertWithMessage("actual is not expected")
+                .that(triangle.triangleType(2, 1, 1))
+                .isEqualTo("Isosceles");
+        assertWithMessage("actual is not expected")
+                .that(triangle.triangleType(3, 4, 5))
+                .isEqualTo("Scalene");
+    }
+
+    @Test
+    public void test5() {
+        Triangle triangle = getNewTriangle();
+        assertWithMessage("actual is not expected")
+                .that(triangle.squaredHypotenuse(3, 4))
+                .isEqualTo(25);
+        assertWithMessage("actual is not expected")
+                .that(triangle.squaredHypotenuse(4, 4))
+                .isEqualTo(32);
+        assertWithMessage("actual is not expected")
+                .that(triangle.squaredHypotenuse(1, 4))
+                .isEqualTo(17);
     }
 
 
